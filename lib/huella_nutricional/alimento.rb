07d5ba6987796fcf67f905cb7c89259d19f23382
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 11:34:29 +0000

    Prueba cantidad terreno funciona

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,8 +1,8 @@
 class Alimento
 
-	attr_accessor :nombre,:kg_gei
+	attr_accessor :nombre,:kg_gei,:area_terreno
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
-		@nombre,@kg_gei= nombre,kg_gei			
+		@nombre,@kg_gei,@area_terreno= nombre,kg_gei,area_terreno			
 	end
 end

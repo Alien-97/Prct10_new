7b1e0d77684b24bc70c0e9a0b5d4820a642f4126
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 16:18:05 +0000

    Prueba metodo que devuelve nombre alimento falla

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,8 +1,8 @@
 class Alimento
 
-	attr_accessor :nombre,:kg_gei,:area_terreno,:proteinas,:carbohidratos,:lipidos,:sexo
+	
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
-		@nombre,@kg_gei,@area_terreno,@proteinas,@lipidos,@sexo= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo			
+		@nombre,@kg_gei,@area_terreno,@proteinas,@carbohidratos,@lipidos,@sexo= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo			
 	end
 end
--- spec/huella_nutricional_spec.rb
+++ spec/huella_nutricional_spec.rb
@@ -8,33 +8,40 @@ RSpec.describe HuellaNutricional do
   		@carne_vaca = Alimento.new("carne de vaca",50.0,164.0,21.1,0.0,3.1,"hombre")
 	end
 
-	context "Probando los getters y el to_s" do
+	context "Probando los attr_accessors" do
 		it "Se ha de poder instanciar un alimento"do
-			expect(@carne_vaca.nombre).to eq("carne de vaca")
+			expect(@carne_vaca.instance_variable_defined?(:@nombre)).to eq(true)
 		end
 
 		it "Se ha de poder instanciar los gases de efecto invernadero"do
-			expect(@carne_vaca.respond_to?("kg_gei")).to eq(true)
+			expect(@carne_vaca.instance_variable_defined?(:@kg_gei)).to eq(true)
 		end
 
 		it"Se ha de poder instanciar los metros cuadrados de terreno"do
-			expect(@carne_vaca.respond_to?("area_terreno")).to eq(true)
+			expect(@carne_vaca.instance_variable_defined?(:@area_terreno)).to eq(true)
 		end
 
 		it"Se han de poder instanciar las proteinas"do
-			expect(@carne_vaca.respond_to?("proteinas")).to eq(true)
+			expect(@carne_vaca.instance_variable_defined?(:@proteinas)).to eq(true)
 		end
 
 		it"Se ha de poder instanciar carbohidratos"do
-			expect(@carne_vaca.respond_to?("carbohidratos")).to eq(true)
-		end
+			expect(@carne_vaca.instance_variable_defined?(:@carbohidratos)).to eq(true)
 
+		end
 		it"Se ha de poder instanciar lipidos"do
-			expect(@carne_vaca.respond_to?("lipidos")).to eq(true)
+			expect(@carne_vaca.instance_variable_defined?(:@lipidos)).to eq(true)
 		end
 
 		it"Se ha de poder instanciar el genero de la persona"do
-			expect(@carne_vaca.respond_to?("sexo")).to eq(true)
+			expect(@carne_vaca.instance_variable_defined?(:@sexo)).to eq(true)
+		end
+
+	end
+
+	context "Probando el metodo devolver nombre atributo"do
+		it"Se ha de poder obtener el nombre del alimento"do
+			expect(@carne_vaca.respond_to?("nombre")).to eq(true)
 		end
 	end
   end

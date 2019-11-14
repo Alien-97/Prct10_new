e1355adbaed0e18aa8ea05f80accae7653bf2383
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 11:27:21 +0000

    Prueba efecto invernadero funciona

--- lib/huella_nutricional.rb
+++ lib/huella_nutricional.rb
@@ -1,5 +1,5 @@
 require "huella_nutricional/version"
-
+require "huella_nutricional/alimento"
 module HuellaNutricional
   class Error < StandardError; end
   # Your code goes here...
--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,15 +1,8 @@
 class Alimento
 
-	attr_accessor :nombre, :kg_gei, :area_terreno,:proteinas,:carbohidratos,:lipidos,:valor_energetico_recomendado
+	attr_accessor :nombre,:kg_gei
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
-		@nombre,@kg_gei,@area_terreno,@proteinas,@carbohidratos,@lipidos,@sexo= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo
-
-		if(sexo.eql?("hombre"))
-			@valor_energetico_recomendado = 3000
-		elsif(sexo.eql?("mujer")) 
-			@valor_energetico_recomendado = 2000
-		end
-			
+		@nombre,@kg_gei= nombre,kg_gei			
 	end
 end

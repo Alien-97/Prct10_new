5f95e5811a3800e69bce63f0dc54fea42a4294c9
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 11:50:44 +0000

    Prueba lipidos funciona

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,8 +1,8 @@
 class Alimento
 
-	attr_accessor :nombre,:kg_gei,:area_terreno,:proteinas,:carbohidratos
+	attr_accessor :nombre,:kg_gei,:area_terreno,:proteinas,:carbohidratos,:lipidos
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
-		@nombre,@kg_gei,@area_terreno,@proteinas= nombre,kg_gei,area_terreno,proteinas,carbohidratos			
+		@nombre,@kg_gei,@area_terreno,@proteinas,@lipidos= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos			
 	end
 end

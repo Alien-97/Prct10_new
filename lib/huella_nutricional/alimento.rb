59b8c3e20fd6099224ccc186e4bcf1e9b276feb2
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 16:23:54 +0000

    Prueba metodo que devuelve kilogramos gases efecto invernadero funciona

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,6 +1,6 @@
 class Alimento
 
-	attr_accessor :nombre
+	attr_accessor :nombre,:kg_gei
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
 		@nombre,@kg_gei,@area_terreno,@proteinas,@carbohidratos,@lipidos,@sexo= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo			

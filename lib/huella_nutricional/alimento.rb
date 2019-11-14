7e625397a6c63c6b6066ecc40af6d090afcc81c8
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 16:19:13 +0000

    Prueba metodo que devuelve nombre alimento funciona

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,6 +1,6 @@
 class Alimento
 
-	
+	attr_accessor :nombre	
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
 		@nombre,@kg_gei,@area_terreno,@proteinas,@carbohidratos,@lipidos,@sexo= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo			

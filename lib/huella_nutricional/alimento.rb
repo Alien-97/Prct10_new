264a9d2827136b1dc9316ecfb26c96c6b211099e
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 11:45:18 +0000

    Prueba carbohidratos funciona

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,8 +1,8 @@
 class Alimento
 
-	attr_accessor :nombre,:kg_gei,:area_terreno,:proteinas
+	attr_accessor :nombre,:kg_gei,:area_terreno,:proteinas,:carbohidratos
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
-		@nombre,@kg_gei,@area_terreno,@proteinas= nombre,kg_gei,area_terreno,proteinas			
+		@nombre,@kg_gei,@area_terreno,@proteinas= nombre,kg_gei,area_terreno,proteinas,carbohidratos			
 	end
 end

cc2eddd68032e7663d37ba2f735b0bc4b57f0372
Alien-97 <alu0101035406@ull.edu.es>
2019-11-14 16:41:16 +0000

    Debe existir el metodo que devuelve el valor energetico del alimento, prueba funciona

--- lib/huella_nutricional/alimento.rb
+++ lib/huella_nutricional/alimento.rb
@@ -1,8 +1,12 @@
 class Alimento
 
-	attr_accessor :nombre,:kg_gei,:area_terreno
+	attr_accessor :nombre,:kg_gei,:area_terreno,:proteinas,:carbohidratos,:lipidos,:sexo
 
 	def initialize(nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo)
 		@nombre,@kg_gei,@area_terreno,@proteinas,@carbohidratos,@lipidos,@sexo= nombre,kg_gei,area_terreno,proteinas,carbohidratos,lipidos,sexo			
+	end
+
+	def valor_energetico_alimento
+		
 	end
 end

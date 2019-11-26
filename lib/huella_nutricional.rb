c50b89040f4d28fd53ab035209c82d6243b211fd
Alien-97 <alu0101035406@ull.edu.es>
2019-11-26 21:41:27 +0000

    Debe existir la clase plato

--- lib/huella_nutricional.rb
+++ lib/huella_nutricional.rb
@@ -1,6 +1,7 @@
 require "huella_nutricional/version"
 require "huella_nutricional/alimento"
 require "huella_nutricional/lista"
+require "huella_nutricional/PlatoHuellaNutricional"
 module HuellaNutricional
   class Error < StandardError; end
   # Your code goes here...

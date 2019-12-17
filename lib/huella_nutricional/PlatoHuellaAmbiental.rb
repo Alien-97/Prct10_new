5f9a916ec0e5bcc88beb6332f30fcecff77f3500
Alien-97 <alu0101035406@ull.edu.es>
2019-12-17 10:32:47 +0000

    añadida documentacion con yard

--- lib/huella_nutricional/PlatoHuellaAmbiental.rb
+++ lib/huella_nutricional/PlatoHuellaAmbiental.rb
@@ -1,15 +1,28 @@
 
+# Clase Plato que calcula la eficiencia energetica del 
+# plato
+#
+# @author Alien Embarec Riadi
+#
+
 
 class PlatoHuellaAmbiental < PlatoHuellaNutricional
 
 	include Comparable
 	
+
+	# Crear un plato (eficiencia energetica)
+	#
+	# @return [PlatoHuellaAmbiental]
 	def initialize(nombre,lista_alimentos,acc_cantidad_alimentos)
 		super(nombre,lista_alimentos,acc_cantidad_alimentos)
 		@emisiones_gei = 0
 		@area_terreno_m2 = 0
 	end
 
+	# Calcular las emisiones de gases de efecto invernadero del plato
+	#
+	# @return @emisiones_gei [Float] emisiones de gases de efecto invernadero del plato
 	def emisiones_gei
 
 		if @emisiones_gei == 0
@@ -26,6 +39,9 @@ class PlatoHuellaAmbiental < PlatoHuellaNutricional
 		@emisiones_gei
 	end
 
+	# Calcular el area de terreno usada en metros cuadrados
+	#
+	# @return @area_terreno_m2 [Float] area de terreno usada en metros cuadrados
 
 	def area_terreno
 
@@ -42,8 +58,14 @@ class PlatoHuellaAmbiental < PlatoHuellaNutricional
 		@area_terreno_m2
 	end
 
+	# (see #to_s)
+	#
+	# Llama al metodo to_s de la clase padre
 	alias parent_to_s to_s
 
+	# Mostrar la salida de la clase formateada
+	#
+	# @return @s [String] la salida de la clase formateada en un String
 
 	def to_s
 		s = ""
@@ -55,11 +77,20 @@ class PlatoHuellaAmbiental < PlatoHuellaNutricional
         s+= "Cantidad de terreno empleado en m2 " + area_terreno.to_s + "\n"
 	end
 
+
+	# Comparar dos platos segun la huella_nutricional
+	# @param other [PlatoHuellaAmbiental] plato con el que se va a comparar
+	# @return  [true,false] el resultado de la comparación
+
 	def <=> (other)
 		if other.is_a? PlatoHuellaNutricional
     		huella_nutricional_plato <=> other.huella_nutricional_plato
     	end
 	end
+
+	# indice de impacto de la energia del plato
+	# 
+	# @return  @ii_energia_plato [Integer] el resultado de la comparación
 
 	def indice_impacto_energia_plato
 
@@ -77,6 +108,10 @@ class PlatoHuellaAmbiental < PlatoHuellaNutricional
 
 	end
 
+	# indice de impacto de la huella de carbono del plato
+	# 
+	# @return  @ii_huella_carbono_plato [Integer] el resultado de la comparación
+
 	def indice_impacto_huella_carbono_plato
 
 		range = [670,830]
@@ -93,6 +128,9 @@ class PlatoHuellaAmbiental < PlatoHuellaNutricional
 
 	end	
 
+	# indice de impacto de la huella nutricional del plato
+	# 
+	# @return  @hn_plato [Integer] la huella nutricional del plato
 
 	def huella_nutricional_plato
 
@@ -101,6 +139,8 @@ class PlatoHuellaAmbiental < PlatoHuellaNutricional
 	end
 
 end
+
+#ejemplos para probar el correcto funcionamiento de la clase
 
 @carne_vaca = Alimento.new("carne de vaca",50.0,164.0,21.1,0.0,3.1,"hombre",100)
 @huevos = Alimento.new("huevos",4.2,5.7,13.0,1.1,11.0,"hombre",100)
